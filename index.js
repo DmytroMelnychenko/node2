b70166e7cb0d44772038cf9d90dc6741a0759ece
DmytroMelnychenko <meldimld@gmail.com>
2022-08-13 20:45:45 +0300

    qwerty

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const fs = require('fs/promises');
 const path = require('path')
 
-const sortGirlsFolder = async () => {
-   console.log(__dirname);
-   await fs.readdir(path.join(__dirname))
+const sortBoysFolder = async () => {
+  try {
+     const folderPath = path.join(__dirname, 'boys');
+     const files =  await fs.readdir(folderPath);
+
+     for (const  file of files) {
+        const pathToFile = path.join(folderPath, file);
+
+        const data = await fs.readFile(pathToFile);
+
+        const user = JSON.parse(data.toString());
+
+         if (user.gender !== 'male') {
+          await fs.rename(pathToFile, path.join(__dirname, 'girls', file));
+         }
+     }
+
+  } catch (e) {
+     console.error(e);
+  }
+
 }
 
-sortGirlsFolder()
+sortBoysFolder('boys', 'male', );
